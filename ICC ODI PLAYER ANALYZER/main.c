42bb18746e289580831d57868421bd438291b554
CHARVI BAPNA <charvibapna27@gmail.com>
2025-11-20 22:33:05 +0530

    Update main.c

--- ICC ODI PLAYER ANALYZER/main.c
+++ ICC ODI PLAYER ANALYZER/main.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
-#include "team.h"
 #include "handlers.h"
+#include "team.h"
 
 int main() {
     initializeData();
 
-    int choice = 0;
-    while (choice != 6) {
-        choice = showMenu();
-        switch (choice) {
-            case 1:
-                handleAddPlayer();
-                break;
-            case 2:
-                handleDisplayTeamPlayers();
-                break;
-            case 3:
-                handleDisplayTeamsBySR();
-                break;
-            case 4:
-                handleDisplayTopK();
-                break;
-            case 5:
-                handleDisplayAllByRole();
-                break;
-            case 6:
-                printf("Exiting... Thank you!\n");
-                break;
-            default:
-                printf("Invalid choice. Please enter a number between 1 and 6.\n");
+    int choice;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Add Player\n");
+        printf("2. Display Players of Team\n");
+        printf("3. Display Teams by Strike Rate\n");
+        printf("4. Display Top K by Role\n");
+        printf("5. Display All by Role\n");
+        printf("0. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: handleAddPlayer(); break;
+            case 2: handleDisplayTeamPlayers(); break;
+            case 3: handleDisplayTeamsBySR(); break;
+            case 4: handleDisplayTopK(); break;
+            case 5: handleDisplayAllByRole(); break;
         }
-    }
+
+    } while(choice != 0);
 
     cleanup();
     return 0;
