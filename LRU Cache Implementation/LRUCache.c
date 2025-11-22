3b714775f2bec6971944454c3becb16c8c05a9ca
CHARVI BAPNA <charvibapna27@gmail.com>
2025-11-22 23:48:44 +0530

    Update LRUCache.c

--- LRU Cache Implementation/LRUCache.c
+++ LRU Cache Implementation/LRUCache.c
@@ -24,3 +24,64 @@ void addToHead(LRUCache* cache, Node* node) {
     if (cache->tail == NULL)
         cache->tail = node;
 }
+void removeNode(LRUCache* cache, Node* node) {
+    if (node->prev)
+        node->prev->next = node->next;
+    else
+        cache->head = node->next;
+
+    if (node->next)
+        node->next->prev = node->prev;
+    else
+        cache->tail = node->prev;
+}
+
+LRUCache* createCache(int capacity) {
+    LRUCache* cache = (LRUCache*)malloc(sizeof(LRUCache));
+    cache->capacity = capacity;
+    cache->size = 0;
+    cache->head = cache->tail = NULL;
+
+    for (int i = 0; i < MAX_HASH; i++)
+        cache->hashMap[i] = NULL;
+
+    return cache;
+}
+
+char* get(LRUCache* cache, int key) {
+    Node* node = cache->hashMap[key];
+
+    if (!node)
+        return NULL;
+
+    removeNode(cache, node);
+    addToHead(cache, node);
+
+    return node->value;
+}
+
+void put(LRUCache* cache, int key, char* value) {
+    Node* node = cache->hashMap[key];
+
+    
+    if (node) {
+        strcpy(node->value, value);
+        removeNode(cache, node);
+        addToHead(cache, node);
+        return;
+    }
+
+   
+    if (cache->size == cache->capacity) {
+        Node* lru = cache->tail;
+        cache->hashMap[lru->key] = NULL;
+        removeNode(cache, lru);
+        free(lru);
+        cache->size--;
+    }
+
+    Node* newNode = createNode(key, value);
+    addToHead(cache, newNode);
+    cache->hashMap[key] = newNode;
+    cache->size++;
+}
